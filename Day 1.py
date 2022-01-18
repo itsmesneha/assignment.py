77ab25e44602de05aa35d25e19679b1b4cd379e8
itsmesneha <87040207+itsmesneha@users.noreply.github.com>
2022-01-18 01:25:40 -0800

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,23 @@
+#creating a list
+items=[]
+
+#checking the datatype
+print(type(items))
+
+#append
+items.append(75)
+items.append(61)
+items.append(23)
+print(items)
+
+#pop
+items.pop()
+print(items)
+
+#insert
+items.insert(0,12)
+print(items)
+
+#remove
+items.remove(75)
+print(items)
