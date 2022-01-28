4f3573c8355a451a1ebd4abb35091d11bd7c6a2a
itsmesneha <87040207+itsmesneha@users.noreply.github.com>
2022-01-28 07:27:16 -0800

    Add files via upload

---
+++ DAY2/script2.js
@@ -0,0 +1,8 @@
+const email = prompt("Enter the email : ");
+const password = prompt("Enter the password : ");
+
+if ((email=="stu@letsupgrade.com") && (password==12345)) {
+    alert("Hurray! You are logged in");
+} else {
+     alert("Try again!!");
+}
