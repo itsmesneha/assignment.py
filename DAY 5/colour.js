e4fc58ce875696fd54492b4dc346eb3b3d5aa31b
itsmesneha <87040207+itsmesneha@users.noreply.github.com>
2022-01-30 00:03:46 -0800

    Add files via upload

---
+++ DAY 5/colour.js
@@ -0,0 +1,20 @@
+function greencolor(){
+    document.getElementById("mydiv").style.backgroundColor='green';
+}
+
+function bluecolor(){
+    document.getElementById("mydiv").style.backgroundColor='blue';
+}
+
+function redcolor(){
+    document.getElementById("mydiv").style.backgroundColor='red';
+}
+
+function yellowcolor(){
+    document.getElementById("mydiv").style.backgroundColor='yellow';
+}
+
+function browncolor(){
+    document.getElementById("mydiv").style.backgroundColor='brown';
+}
+
